a3b97b0654db88b3fad45b5ba7df540b52076b1c
Hussein Ayoub <Hussein-Ayoub-96@hotmail.com>
2021-06-02 16:43:08 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,28 @@
+"Basic Queries"
+
+
+"SELECT Name FROM students"
+
+"SELECT * FROM students
+WHERE Age > 30"
+
+"SELECT * from students
+WHERE Gender= "F"
+AND Age > 30"
+
+"SELECT Points FROM students
+where Name = "Alex" "
+
+"INSERT INTO students 
+VALUES (7, "Hussein", 24, "M", 1000 );  "
+
+"UPDATE students
+SET Points= 500
+--WHERE name = "Basma"; "
+
+"UPDATE students
+SET Points= 100
+WHERE name = "Alex"; "
+
+
+
